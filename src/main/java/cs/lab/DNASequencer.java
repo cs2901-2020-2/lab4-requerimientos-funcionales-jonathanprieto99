6fd6b870f4141753fc9efb65ef4511a58e55abfb
Jonathan Prieto <jonathan.prieto@me.com>
2020-09-16 14:58:13 -0500

    Version Funcional

--- src/main/java/cs/lab/DNASequencer.java
+++ src/main/java/cs/lab/DNASequencer.java
@@ -11,34 +11,92 @@ public class  DNASequencer {
     }
 
     public String calculate(List<String> part){
-        String ResultadoSecuencia="";
-        for (int i = 0; i < part.size(); i++) {
-            if (ResultadoSecuencia.equals("")){
-                ResultadoSecuencia=part.get(i);
+
+        String cadenaresultante = "";
+
+        for (String s : part) {
+            if (cadenaresultante.length() == 0) {
+                cadenaresultante = s;
+            } else {
+                cadenaresultante = imprimiradn(cadenaresultante, s);
             }
-            else {
-                String comun="";
-                for (int j = 0; j < ResultadoSecuencia.length(); j++) {
-                    //logger.info("Word1: "+ResultadoSecuencia);
-                    for (int k = 0; k < part.get(i).length(); k++) {
-                        //logger.info("Word2: "+part.get(i));
-                        if(ResultadoSecuencia.charAt(j)==part.get(i).charAt(k)){
-                            comun+=ResultadoSecuencia.charAt(j);
-                            break;
-                        }
-                    }
+
+        }
+
+        logger.info(cadenaresultante);
+        return cadenaresultante;
+
+    }
+
+    public String reverse(String palabra)
+    {
+        StringBuilder sb = new StringBuilder(palabra);
+        return (sb.reverse().toString());
+    }
+
+    public String imprimiradn(String palabra1, String palabra2){
+        int sizeword1=palabra1.length();
+        int sizeword2=palabra2.length();
+        int [][] matriz = new int[sizeword1+1][sizeword2+1];
+
+        for (int i = 0; i < sizeword1; ++i){
+            for (int j = 0; j <= sizeword2; ++j){
+                if(i == 0){
+                    matriz[i][j] = j;
                 }
-                logger.info("Comun: "+comun);
-                logger.info("Principal: "+ResultadoSecuencia);
-                logger.info("Agregar: "+part.get(i));
-                ResultadoSecuencia=ResultadoSecuencia.concat(part.get(i));
-                logger.info("Concatenado: "+ResultadoSecuencia);
-                ResultadoSecuencia=ResultadoSecuencia.replace(comun,"");
-                logger.info("Esta Aca: "+ResultadoSecuencia);
+                else if(j == 0){
+                    matriz[i][j] = i;
+                }
+                else if(palabra1.charAt(i-1) == palabra2.charAt(j-1)){
+                    matriz[i][j] = 1 + matriz[i-1][j-1];
+                }
+                else{
+                    matriz[i][j] = 1 + Math.min(matriz[i - 1][j], matriz[i][j - 1]);
+                }
             }
         }
-        logger.info("Final: "+ResultadoSecuencia);
-        return ResultadoSecuencia;
+
+        StringBuilder palabraresultante= new StringBuilder();
+        int tamanowhile1 = sizeword1;
+        int tamanowhile2 = sizeword2;
+
+        while (tamanowhile1 > 0 && tamanowhile2 > 0)
+        {
+            if (palabra1.charAt(tamanowhile1 - 1) == palabra2.charAt(tamanowhile2 - 1))
+            {
+                palabraresultante.append(palabra1.charAt(tamanowhile1 - 1));
+                --tamanowhile1;
+                --tamanowhile2;
+            }
+
+            else if (matriz[tamanowhile1 - 1][tamanowhile2] > matriz[tamanowhile1][tamanowhile2 - 1])
+            {
+                palabraresultante.append(palabra2.charAt(tamanowhile2 - 1));
+                --tamanowhile2;
+            }
+
+            else
+            {
+                palabraresultante.append(palabra1.charAt(tamanowhile1 - 1));
+                --tamanowhile1;
+            }
+        }
+
+        while (tamanowhile1 > 0)
+        {
+            palabraresultante.append(palabra1.charAt(tamanowhile1 - 1));
+            --tamanowhile1;
+        }
+
+        while (tamanowhile2 > 0)
+        {
+            palabraresultante.append(palabra2.charAt(tamanowhile2 - 1));
+            --tamanowhile2;
+        }
+
+        palabraresultante = new StringBuilder(reverse(palabraresultante.toString()));
+        logger.info(palabraresultante.toString());
+        return palabraresultante.toString();
     }
 }
 
